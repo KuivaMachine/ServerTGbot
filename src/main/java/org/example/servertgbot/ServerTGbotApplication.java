7c2760dfc3f437bdba0cc87376321bd274da77cf
olegz <OLEGZAOSTROVTSEV19@yandex.ru>
2025-02-25 17:43:02 +0300

    s

--- src/main/java/org/example/servertgbot/ServerTGbotApplication.java
+++ src/main/java/org/example/servertgbot/ServerTGbotApplication.java
@@ -1,11 +1,13 @@
 package org.example.servertgbot;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.log4j.Log4j2;
 import org.example.configuration.TelegramBotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@Log4j2
 @SpringBootApplication
 @EnableConfigurationProperties({TelegramBotConfig.class})
 public class ServerTGbotApplication {
@@ -14,6 +16,7 @@ public class ServerTGbotApplication {
         Dotenv dotenv = Dotenv.configure().load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(ServerTGbotApplication.class, args);
+        log.info("ЭТО НОВАЯ ВЕРСИЯ");
     }
 
 }
